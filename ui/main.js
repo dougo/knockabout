9b684c2aeda8f9b5bcdeef308a685c44451c0a09
DougOrleans <DougOrleans@c10eb117-c019-0410-a1aa-73a00b9cc101>
2006-08-26 04:20:05 +0000

    Many changes while on vacation.

--- ui/main.js
+++ ui/main.js
@@ -1,39 +1,85 @@
 svgNS = "http://www.w3.org/2000/svg";
 xlinkNS = "http://www.w3.org/1999/xlink";
-board = document.getElementById("board");
+PaleGreen3 = "#7CCD7C";
+SQRT3 = Math.sqrt(3);
+SQRT5 = Math.sqrt(5);
 
-// Use a node by name.
-function useNode(name) {
-  var node = document.createElementNS(svgNS, "use");
-  node.setAttributeNS(xlinkNS, "href", "#" + name);
+// Hexagon centered at the origin, height = 2, width = sqrt(5)/2 + 1,
+// each edge is sqrt(5)/2.
+hexPoints =
+  SQRT5/4        + ",-1 " +	// northeast corner
+  (SQRT5/4+1/2)  + ",0 " +	// east
+  SQRT5/4        + ",1 " +	// southeast
+  -SQRT5/4       + ",1 " +	// southwest
+  -(SQRT5/4+1/2) + ",0 " +	// west
+  -SQRT5/4       + ",-1";	// northwest
+
+// Die shapes that fit into the height-2 hexagon centered at the origin.
+trianglePoints = "0,-0.85 0.7,0.5 -0.7,0.5";
+squarePoints = "-0.65,-0.65 0.65,-0.65 0.65,0.65 -0.65,0.65";
+diamondPoints = "0,-0.9 0.7,0 0,0.9 -0.7,0";
+
+function makeGroup(parent) {
+  return parent.appendChild(document.createElementNS(svgNS, "g"));
+}
+
+function makeHex() {
+  var hex = document.createElementNS(svgNS, "polygon");
+  hex.setAttribute("points", hexPoints);
+  return hex;
+}
+
+function moveXY(node, x, y) {
+  node.setAttribute("transform", "translate(" + x + "," + y + ")");
   return node;
 }
 
-// Add an item to the board.
-function addItem(item, row, col) {
-  var x = 200 + 265*col;
-  var y = 1100 + 153*(2*row - col);
-  item.setAttribute("transform", "translate(" + x + "," + y + ")");
-  board.appendChild(item);
-  return item;
+function moveRC(node, row, col) {
+  return moveXY(node, (SQRT5/4+1.05)*col, 2*row - col);
+  // TO DO: why the .05??
 }
 
-function addSpace(row, col) {
-  addItem(useNode("space"), row, col);
+function isGutterRC(row, col) {
+  return (row == 0 || row == 12 || col == 0 || col == 12 ||
+	  row-col == 6 || row-col == -6);
 }
 
-for (col = 0; col < 13; col++) {
-  for (row = Math.max(0, col - 6); row < Math.min(col + 7, 13); row++) {
-    addSpace(row, col);
+function makeBoard(parent) {
+  var board = makeGroup(parent);
+  moveXY(board, 6, 10);
+  var gutter = makeHex();
+  gutter.setAttribute("transform", "translate(10,6) rotate(30) scale(11)");
+  // TO DO: why is the translate necessary?  Shouldn't it inherit from board?
+  // And why are the coordinates reversed?!
+  // And shouldn't the scale only need to be like 7?
+  gutter.setAttribute("stroke", "black");
+  gutter.setAttribute("stroke-width", 0.04);
+  gutter.setAttribute("fill-opacity", 0);
+  board.appendChild(gutter);
+
+  for (col = 0; col < 13; col++) {
+    for (row = Math.max(0, col - 6); row < Math.min(col + 7, 13); row++) {
+      var hex = makeHex();
+      hex.setAttribute("fill", PaleGreen3);
+      hex.setAttribute("stroke", "black");
+      hex.setAttribute("stroke-width", 0.1);
+      moveRC(hex, row, col);
+      if (isGutterRC(row, col))
+	// Gutter space, shrink to half size.
+	hex.setAttribute("transform", hex.getAttribute("transform")
+			 + "scale(0.5)");
+      board.appendChild(hex);
+    }
   }
-}
 
+  return board;
+}
 
 function dieShapePoints(size) {
   switch (size) {
-  case 4: return "   0,-153  132,76   -132,76";           // triangle
-  case 6: return "-100,-100  100,-100  100,100 -100,100"; // square
-  case 8: return "   0,-153  100,0       0,153 -100,0";   // diamond
+  case 4: return trianglePoints;
+  case 6: return squarePoints;
+  case 8: return diamondPoints;
   }
 }
 
@@ -43,8 +89,8 @@ function makeDieShape(owner, size) {
   if (owner == 0) {
     shape.setAttribute("fill", "yellow");
     shape.setAttribute("stroke", "black");
-    shape.setAttribute("stroke-width", 3);
   }
+  shape.setAttribute("stroke-width", 0.1);
   return shape;
 }
 
@@ -53,38 +99,49 @@ function makeDieText(owner, face) {
   text.setAttribute("text-anchor", "middle");
   text.setAttribute("font-family", "sans-serif");
   text.setAttribute("font-weight", "bold");
-  text.setAttribute("font-size", 120);
-  text.setAttribute("y", 45);
+  text.setAttribute("font-size", 0.9);
+  text.setAttribute("y", 0.3);
+  // TO DO: figure out text color from background color,
+  // i.e. light on dark or dark on light
   if (owner == 1) text.setAttribute("fill", "white");
   text.textContent = face;
   return text;
 }
 
-function makeDie(owner, row, col, size, face) {
-  var die = document.createElementNS(svgNS, "g");
-  var shape = makeDieShape(owner, size);
-  die.appendChild(shape);
-  var text = makeDieText(owner, face);
-  die.appendChild(text);
-  addItem(die, row, col);
+function makeDice(parent) {
+  var dice = makeGroup(parent);
+
+  function makeDie(owner, row, col, size, face) {
+    var die = document.createElementNS(svgNS, "g");
+    die.appendChild(makeDieShape(owner, size));
+    die.appendChild(makeDieText(owner, face));
+    moveRC(die, row, col);
+    return dice.appendChild(die);
+  }
+
+  makeDie(0, 4, 3, 4, 1);
+  makeDie(0, 5, 5, 4, 1);
+  makeDie(0, 6, 7, 4, 1);
+  makeDie(0, 7, 9, 4, 1);
+  makeDie(0, 3, 4, 6, 2);
+  makeDie(0, 4, 6, 6, 2);
+  makeDie(0, 5, 8, 6, 2);
+  makeDie(0, 2, 5, 8, 3);
+  makeDie(0, 3, 7, 8, 3);
+
+  makeDie(1, 5, 3, 4, 1);
+  makeDie(1, 6, 5, 4, 1);
+  makeDie(1, 7, 7, 4, 1);
+  makeDie(1, 8, 9, 4, 1);
+  makeDie(1, 7, 4, 6, 2);
+  makeDie(1, 8, 6, 6, 2);
+  makeDie(1, 9, 8, 6, 2);
+  makeDie(1, 9, 5, 8, 3);
+  makeDie(1, 10, 7, 8, 3);
+
+  return dice;
 }
 
-makeDie(0, 4, 3, 4, 1);
-makeDie(0, 5, 5, 4, 1);
-makeDie(0, 6, 7, 4, 1);
-makeDie(0, 7, 9, 4, 1);
-makeDie(0, 3, 4, 6, 2);
-makeDie(0, 4, 6, 6, 2);
-makeDie(0, 5, 8, 6, 2);
-makeDie(0, 2, 5, 8, 3);
-makeDie(0, 3, 7, 8, 3);
-
-makeDie(1, 5, 3, 4, 1);
-makeDie(1, 6, 5, 4, 1);
-makeDie(1, 7, 7, 4, 1);
-makeDie(1, 8, 9, 4, 1);
-makeDie(1, 7, 4, 6, 2);
-makeDie(1, 8, 6, 6, 2);
-makeDie(1, 9, 8, 6, 2);
-makeDie(1, 9, 5, 8, 3);
-makeDie(1, 10, 7, 8, 3);
+root = document.documentElement;
+board = makeBoard(root);
+dice = makeDice(board);
