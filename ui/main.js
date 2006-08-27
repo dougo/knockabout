57e123213136cd2df89aa8c163460ab5af2df27d
DougOrleans <DougOrleans@c10eb117-c019-0410-a1aa-73a00b9cc101>
2006-08-27 15:34:17 +0000

    Fixed the hex and die dimensions, plus some other tweaks.

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,30 @@
 svgNS = "http://www.w3.org/2000/svg";
 xlinkNS = "http://www.w3.org/1999/xlink";
 PaleGreen3 = "#7CCD7C";
+PaleGreen4 = "#548B54";
+SQRT2 = Math.sqrt(2);
 SQRT3 = Math.sqrt(3);
-SQRT5 = Math.sqrt(5);
 
-// Hexagon centered at the origin, height = 2, width = sqrt(5)/2 + 1,
-// each edge is sqrt(5)/2.
+// The unit hexagon is a regular hexagon centered at the origin with
+// the flat side up, circumscribed by the unit circle: width = 2,
+// height = sqrt(3).
 hexPoints =
-  SQRT5/4        + ",-1 " +	// northeast corner
-  (SQRT5/4+1/2)  + ",0 " +	// east
-  SQRT5/4        + ",1 " +	// southeast
-  -SQRT5/4       + ",1 " +	// southwest
-  -(SQRT5/4+1/2) + ",0 " +	// west
-  -SQRT5/4       + ",-1";	// northwest
+  "-1,0 -0.5," + SQRT3/2 + " 0.5," + SQRT3/2 +
+  " 1,0 0.5," + -SQRT3/2 + " -0.5," + -SQRT3/2;
 
-// Die shapes that fit into the height-2 hexagon centered at the origin.
-trianglePoints = "0,-0.85 0.7,0.5 -0.7,0.5";
-squarePoints = "-0.65,-0.65 0.65,-0.65 0.65,0.65 -0.65,0.65";
-diamondPoints = "0,-0.9 0.7,0 0,0.9 -0.7,0";
+// Die shapes that are circumscribed by the unit hexagon, leaving room
+// for 0.1 stroke-width.
+trianglePoints =
+  "0," + (-SQRT3/2+0.1) + " 0.7," + (SQRT3/4-0.05) + " -0.7, " + (SQRT3/4-0.05);
+squareInradius = SQRT3/SQRT2/2-0.05;
+squarePoints =
+  -squareInradius + "," + -squareInradius + " " + 
+   squareInradius + "," + -squareInradius + " " +
+   squareInradius + "," +  squareInradius + " " +
+  -squareInradius + "," +  squareInradius;
+diamondPoints =
+  "0," + (-SQRT3/2+0.1) + " " + "0.5,0 " +
+  "0," + (SQRT3/2-0.1) + " " + "-0.5,0";
 
 function makeGroup(parent) {
   return parent.appendChild(document.createElementNS(svgNS, "g"));
@@ -34,9 +41,11 @@ function moveXY(node, x, y) {
   return node;
 }
 
+// Convert board (row,col) coordinates into (x,y) coordinates.
+// The center of the board is at the origin, i.e. (6,6) -> (0,0).
 function moveRC(node, row, col) {
-  return moveXY(node, (SQRT5/4+1.05)*col, 2*row - col);
-  // TO DO: why the .05??
+  col -= 6; row -= 6;
+  return moveXY(node, 1.5*col, SQRT3/2*(2*row - col));
 }
 
 function isGutterRC(row, col) {
@@ -46,13 +55,10 @@ function isGutterRC(row, col) {
 
 function makeBoard(parent) {
   var board = makeGroup(parent);
-  moveXY(board, 6, 10);
+  moveXY(board, 15, 15);
   var gutter = makeHex();
-  gutter.setAttribute("transform", "translate(10,6) rotate(30) scale(11)");
-  // TO DO: why is the translate necessary?  Shouldn't it inherit from board?
-  // And why are the coordinates reversed?!
-  // And shouldn't the scale only need to be like 7?
-  gutter.setAttribute("stroke", "black");
+  gutter.setAttribute("transform", "rotate(30) scale(" + 6*SQRT3 + ")");
+  gutter.setAttribute("stroke", PaleGreen4);
   gutter.setAttribute("stroke-width", 0.04);
   gutter.setAttribute("fill-opacity", 0);
   board.appendChild(gutter);
@@ -62,7 +68,7 @@ function makeBoard(parent) {
       var hex = makeHex();
       hex.setAttribute("fill", PaleGreen3);
       hex.setAttribute("stroke", "black");
-      hex.setAttribute("stroke-width", 0.1);
+      hex.setAttribute("stroke-width", 0.075);
       moveRC(hex, row, col);
       if (isGutterRC(row, col))
 	// Gutter space, shrink to half size.
@@ -86,11 +92,12 @@ function dieShapePoints(size) {
 function makeDieShape(owner, size) {
   var shape = document.createElementNS(svgNS, "polygon");
   shape.setAttribute("points", dieShapePoints(size));
+  shape.setAttribute("stroke", "black");
+  // TO DO: let user choose colors
   if (owner == 0) {
-    shape.setAttribute("fill", "yellow");
-    shape.setAttribute("stroke", "black");
+    shape.setAttribute("fill", "gold");
   }
-  shape.setAttribute("stroke-width", 0.1);
+  shape.setAttribute("stroke-width", 0.05);
   return shape;
 }
 
@@ -99,8 +106,8 @@ function makeDieText(owner, face) {
   text.setAttribute("text-anchor", "middle");
   text.setAttribute("font-family", "sans-serif");
   text.setAttribute("font-weight", "bold");
-  text.setAttribute("font-size", 0.9);
-  text.setAttribute("y", 0.3);
+  text.setAttribute("font-size", 0.7);
+  text.setAttribute("y", 0.2);
   // TO DO: figure out text color from background color,
   // i.e. light on dark or dark on light
   if (owner == 1) text.setAttribute("fill", "white");
